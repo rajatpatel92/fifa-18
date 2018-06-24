c029292d256ef86308c9d48b322c13b298aa2d6e
Rajat Patel <rajat@thenextgeek.com>
2018-06-24 16:07:45 +0530

    Firebase CRUD operations complete - Points calculation logic added - Purge test database - Added Validations to prevent illegal additions

--- vue-app.js
+++ vue-app.js
@@ -124,6 +124,7 @@ var db = firebaseApp.database()
 
 var playersRef = db.ref('players')
 var predictionsRef = db.ref('predictions')
+var datesRef = db.ref('dates')
 
 // first argument is the source array, followed by one or more property names
 var pluckMany = function() {
@@ -149,11 +150,16 @@ var app = new Vue({
 		loading: true,
 		predictions: [],
 		prediction: {},
-		message: ''
+		message: '',
+		showRefresh: false,
+		pointsUpdatedOnce: false,
+		savedOnce: false,
+		hasError: false
 	},
 	firebase: {
 		players: playersRef,
-		fbpredictions: predictionsRef
+		fbpredictions: predictionsRef,
+		dates: datesRef
 	},
 	mounted() {
 		axios
@@ -170,7 +176,14 @@ var app = new Vue({
 		'userCode':  {
 			handler:function (val, oldVal){
 				if (val.trim()){
-					this.message = '';
+					var playerName = _.findWhere(this.players, {userCode:val.trim()});
+					if (playerName) {
+						this.message = '';
+						this.hasError = false;
+					} else {
+						this.message = "No player found with entered Code. Please enter correct code."
+						this.hasError = true;
+					}
 				}
 			}
 		}
@@ -180,6 +193,10 @@ var app = new Vue({
 			var today = new Date();
 			var thisMonth = parseInt(today.getMonth());
 			return today.getFullYear() +"-0"+ parseInt(thisMonth+1) + "-" + today.getDate();
+		},
+		defaultMatchStart: function(){
+			var today = new Date();
+			return new Date(today.getFullYear(), today.getMonth(), today.getDate(), 17, 15, 0, 0).getTime();
 		},
 		todayPredictions: function() {
 			//Get Today Date
@@ -227,24 +244,30 @@ var app = new Vue({
 		},
 		onTeamSelect: function (prediction) {
 			if (this.userCode) {
-				prediction.userCode = this.userCode;
-				_.each(this.predictions, function (item) {
-					if (prediction.match == item.match) {
-						if (item.userCode) {
-							item.userCode = this.userCode;
+				var playerName = _.findWhere(this.players, {userCode: this.userCode});
+				if (playerName){
+					prediction.userCode = this.userCode;
+					_.each(this.predictions, function (item) {
+						if (prediction.match == item.match) {
+							if (item.userCode) {
+								item.userCode = this.userCode;
+							}
+							item.guess = prediction.guess;
+							item.date = prediction.date;
+							item.userCode = prediction.userCode;
+							return;
 						}
-						item.guess = prediction.guess;
-						item.date = prediction.date;
-						item.userCode = prediction.userCode;
-						return;
+					});
+					if (this.predictions.length < 3) {
+						this.predictions.push(prediction);
 					}
-				});
-				if (this.predictions.length < 3) {
-					this.predictions.push(prediction);
+					console.log(this.predictions);
+				} else {
+					this.hasError = true;
+					this.message = "No player found with entered Code. Please enter correct code.";
 				}
-				console.log(this.predictions);
 			} else {
-				this.message = "Please enter your User Code!!"
+				this.message = "Please enter your User Code before saving!!"
 			}
 		},
 		writePredictionData: function (userid, predictions) {
@@ -254,53 +277,136 @@ var app = new Vue({
 			}
 		},
 		saveIt: function () {
-			if (!this.userCode) {
-				this.message = "Please enter User Code and Try Again!";
-			} else {
-				_.each(this.predictions, function(prediction) {
-					if (!prediction.userCode) {
-						this.message = "Please enter User Code and Try Again!";
-					} else if (!prediction.guess) {
-						this.message = "Please make all predictions before saving";
+			if (!this.savedOnce) {
+				if (!this.userCode) {
+					this.hasError = true;
+					this.message = "Please enter User Code and Try Again!";
+				} else {
+					var playerName = _.findWhere(this.players, {userCode: this.userCode});
+					if (playerName){
+						_.each(this.predictions, function(prediction) {
+							if (!prediction.userCode) {
+								this.hasError = true;
+								this.message = "Please enter User Code and Try Again!";
+							} else if (!prediction.guess) {
+								this.hasError = true;
+								this.message = "Please make all predictions before saving";
+							}
+						});
+					} else {
+						this.hasError = true;
+						this.message = "No player found with entered Code. Please enter correct code."
 					}
-				});
-			}
-			_.each(this.predictions, function(pred) {
-				predictionsRef.push({
-					userCode: pred.userCode,
-					match: pred.match,
-					guess: pred.guess,
-					date: pred.date
-				})
-			});
-			this.message = "Saved Successfully..!!";
-		},
-		calculatePoints: function (allpreds) {
-			//Go through yesterday's predictions and calculate points
-			var yesterdayWinners = [];
-			_.each(this.yesterdayResults, function (yesterdayMatch) {
-				var winner = {
-					winner: app.getWinningTeam(yesterdayMatch),
-					match: yesterdayMatch.homeTeamName +"-"+ yesterdayMatch.awayTeamName
 				}
-				yesterdayWinners.push(winner);
-				winner = {};
-			});
-			
-			var today = new Date();
-			var thisMonth = parseInt(today.getMonth());
-			var todayDate = today.getFullYear() +"-0"+ parseInt(thisMonth+1) + "-" + today.getDate();
-			
-			var yesterdayDate = null;
-			if (this.todayDate === 1) {
-				yesterdayDate = today.getFullYear() +'-'+ today.getMonth() +'-'+ '30';
+
+				if ((new Date()).getTime() > this.defaultMatchStart) {
+					this.hasError = true;
+					this.message = "Submission for today has been closed at 5:15 pm.";
+				}
+
+				//Make sure there is no error and player has been identified
+				if (!this.hasError && playerName) {
+					_.each(this.predictions, function(pred) {
+						predictionsRef.push({
+							userCode: pred.userCode,
+							playerName: playerName.name,
+							match: pred.match,
+							guess: pred.guess,
+							date: pred.date,
+							time: (new Date()).getTime()
+						})
+					});
+					this.message = "Saved Successfully..!!";
+					this.savedOnce = true;
+				}
+
 			} else {
-				yesterdayDate = today.getFullYear() +'-0'+ parseInt(thisMonth+1) +'-'+ parseInt(today.getDate()-1);
+				this.message = "You cannot make choices twice a day buddy..!!";
+				this.hasError = true;
 			}
-			var yesterdayPredictions = _.filter(this.fbpredictions, function (prediction){ return prediction.date.includes(yesterdayDate) });
-			
-			console.log(yesterdayPredictions);
-			console.log(yesterdayWinners);
+		},
+		updatePlayers: function (item) { 
+			// create a copy of the item
+			const copy = {...item}
+			// remove the .key attribute
+			delete copy['.key']
+			playersRef.child(item['.key']).set(copy)
+		}, 
+		updateDates: function (item) {
+			// create a copy of the item
+			const copy = {...item}
+			// remove the .key attribute
+			delete copy['.key']
+			datesRef.child(item['.key']).set(copy)
+		},
+		calculatePoints: function () {
+			var lastRefreshDate = this.dates;
+			console.log("lastrefdate");
+			console.log(lastRefreshDate[0][".value"]);
+			if (lastRefreshDate[0][".value"] !== this.todayDate && !this.pointsUpdatedOnce) {
+				//Go through yesterday's predictions and calculate points
+				var yesterdayWinners = [];
+				var players = this.players;
+				_.each(this.yesterdayResults, function (yesterdayMatch) {
+					var winner = {
+						winner: app.getWinningTeam(yesterdayMatch),
+						match: yesterdayMatch.homeTeamName +"-"+ yesterdayMatch.awayTeamName
+					}
+					yesterdayWinners.push(winner);
+					winner = {};
+				});
+				
+				var today = new Date();
+				var thisMonth = parseInt(today.getMonth());
+				var todayDate = today.getFullYear() +"-0"+ parseInt(thisMonth+1) + "-" + today.getDate();
+				
+				var yesterdayDate = null;
+				if (this.todayDate === 1) {
+					yesterdayDate = today.getFullYear() +'-'+ today.getMonth() +'-'+ '30';
+				} else {
+					yesterdayDate = today.getFullYear() +'-0'+ parseInt(thisMonth+1) +'-'+ parseInt(today.getDate()-1);
+				}
+				var yesterdayPredictions = _.filter(this.fbpredictions, function (prediction){ return prediction.date.includes(yesterdayDate) });
+
+				_.each(yesterdayWinners, function (winner) {
+					//Filter current match
+					var matchPreds = _.filter(yesterdayPredictions, function (pred) { return pred.match === winner.match });
+					console.log(matchPreds);
+					//Count points to be credited to winner
+					var counts = _.countBy(matchPreds, function (pred){ return pred.guess == winner.winner ? 'winningPlayers' : 'losingPlayers' });
+					console.log(counts);
+					var winningPlayers = _.pluck(_.filter(matchPreds, function(pred) { return pred.guess == winner.winner }), 'userCode' );
+					console.log(winningPlayers);
+					//Add points to winner account
+					console.log("Before points addition");
+					console.log(players);
+					_.each(winningPlayers, function (winningPlayer) {
+						var player = _.findWhere(players, { userCode: winningPlayer});
+						if (player && counts["losingPlayers"]) {
+							player.points += parseInt(counts["losingPlayers"]);
+						}
+					});
+					console.log("After points addition")
+					console.log(players);
+					
+				});
+
+				_.each(this.players, function (player) {
+					app.updatePlayers(player);
+				});
+
+				_.each(lastRefreshDate, function(date){
+					date[".value"] = app.todayDate;
+					app.updateDates(date);
+				});
+
+				this.pointsUpdatedOnce = true;
+				
+				console.log(yesterdayPredictions);
+				console.log(yesterdayWinners);
+			} else {
+				console.log("Skipping calculation - Point calculation last date is same as today's date")
+			}
 		}
 	}
 });
